a37b491073cdca29bbcc24c420e3d23a319bfe61
pampalone <erik.pampalone@gmail.com>
2013-12-07 10:48:43 -0800

    new file:   .DS_Store 	new file:   .gitignore 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/.DS_Store 	new file:   app/assets/images/.keep 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/javascripts/posts.js.coffee 	new file:   app/assets/javascripts/welcome.js.coffee 	new file:   app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/posts.css.scss 	new file:   app/assets/stylesheets/welcome.css.scss 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/concerns/.keep 	new file:   app/controllers/posts_controller.rb 	new file:   app/controllers/welcome_controller.rb 	new file:   app/helpers/application_helper.rb 	new file:   app/helpers/posts_helper.rb 	new file:   app/helpers/welcome_helper.rb 	new file:   app/mailers/.keep 	new file:   app/models/.keep 	new file:   app/models/concerns/.keep 	new file:   app/views/.DS_Store 	new file:   app/views/layouts/application.html.erb 	new file:   app/views/welcome/index.html.erb 	new file:   bin/bundle 	new file:   bin/rails 	new file:   bin/rake 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/filter_parameter_logging.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/seeds.rb 	new file:   lib/assets/.keep 	new file:   lib/tasks/.keep 	new file:   log/.keep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/robots.txt 	new file:   test/controllers/.keep 	new file:   test/controllers/posts_controller_test.rb 	new file:   test/controllers/welcome_controller_test.rb 	new file:   test/fixtures/.keep 	new file:   test/helpers/.keep 	new file:   test/helpers/posts_helper_test.rb 	new file:   test/helpers/welcome_helper_test.rb 	new file:   test/integration/.keep 	new file:   test/mailers/.keep 	new file:   test/models/.keep 	new file:   test/test_helper.rb 	new file:   vendor/assets/javascripts/.keep 	new file:   vendor/assets/stylesheets/.keep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'd779277ac268341c9147033b69f3ec7c0fc76f74151a375e437c15ce04e8056a7dcc7abeae5cb7b59e6e98b1659b96bea9a238199bbc5171856e63ebfc7241e2'
